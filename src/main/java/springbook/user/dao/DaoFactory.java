3fce839eaae2c7d967f7d103b89edcbaacf85512
kimmari10 <kimmari10@naver.com>
2014-10-10 14:19:20 +0900

    1.4.2 오브젝트 팩토리의 활용 p.92 DaoFactory에서 여러가지 Dao를 사용하더라도 ConnectionMaker를 한번만 사용하도록 수정 Account와 MessageDao는 구현하지 않았으므로 주석처리 함

--- src/main/java/springbook/user/dao/DaoFactory.java
+++ src/main/java/springbook/user/dao/DaoFactory.java
@@ -2,9 +2,22 @@ package springbook.user.dao;
 
 public class DaoFactory {
 	public UserDao userDao() {
-		ConnectionMaker connectionMaker = new DConnectionMaker();
-		UserDao userDao = new UserDao(connectionMaker);
-		return userDao;
+		return new UserDao(connectionMaker());
 	}
+	
+	/*public AccountDao accountDao() {
+		return new AccountDao(connectionMaker());
+	}
+	
+	public MessageDao messageDao() {
+		return new MessageDao(connectionMaker());
+	}*/
+	// AccountDao와 MessageDao를 실제로 구현하지 않았으므로 주석처리 함
+	
+	public ConnectionMaker connectionMaker() {
+		return new DConnectionMaker();
+	}
+	
+	
 	
 }
