888fc6ca1ca81938d9e655a7037427db84304c39
kimmari10 <kimmari10@naver.com>
2014-10-10 15:34:10 +0900

    1.7.4 의존관계 주입의 응용 Connection 할때마다 count해주는 기능 추가

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -5,6 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import springbook.user.domain.User;
 
 
@@ -15,14 +17,20 @@ public class UserDao {
 	private Connection c;
 	private User user;
 	
+	public UserDao() {
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(DaoFactory.class);
+		this.connectionMaker = context.getBean("connectionMaker", ConnectionMaker.class);
+	}
+	
 	public UserDao(ConnectionMaker connectionMaker) {
 		this.connectionMaker = connectionMaker;
 	}
 	
-	public static synchronized UserDao getInstance() {
+/*	public static synchronized UserDao getInstance() {
 		if (INSTANCE == null) INSTANCE = new UserDao(???);
 		return INSTANCE;
-	}
+	}*/
 	
 	public void add(User user) throws ClassNotFoundException, SQLException {
 		this.c = connectionMaker.makeConnection();
