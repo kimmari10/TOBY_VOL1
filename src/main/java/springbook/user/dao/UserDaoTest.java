1e62212ee140e05b400037f4bbc598f49d9bb7a8
kimmari10 <kimmari10@naver.com>
2014-10-11 18:26:23 +0900

    2.2.2 테스트의 효율적인 수행과 결과 관리 p.158 UserDaoTest Test애노테이션 추가해서 JUnit을 이용한 테스트 실행 Default package에서 springbook.user.dao 패키리로 클래스이동

--- src/main/java/springbook/user/dao/UserDaoTest.java
+++ src/main/java/springbook/user/dao/UserDaoTest.java
@@ -1,5 +1,47 @@
 package springbook.user.dao;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.sql.SQLException;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import springbook.user.domain.User;
+
 
 public class UserDaoTest {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		JUnitCore.main("springbook.user.dao.UserDaoTest");
+	}
+	
+	@Test
+	public void addAndGet() throws ClassNotFoundException, SQLException {
+		ApplicationContext context = 
+				new GenericXmlApplicationContext("springbook/user/dao/applicationContext.xml");
+
+		
+		UserDao dao = context.getBean("userDao", UserDao.class);
+		User user1 = new User();
+		
+		user1.setId("gyumee");
+		user1.setName("박성철");
+		user1.setPassword("springno1");
+		
+		dao.add(user1);
+		
+		User user2 = dao.get(user1.getId());
+		
+		assertThat(user2.getName(), is(user1.getName()));
+		assertThat(user2.getPassword(), is(user1.getPassword()));
+		
+		
+	}
 
 }
