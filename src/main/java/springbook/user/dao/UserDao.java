695a6f8fcb9d63b36cac23c23a78e8151a47bcfd
kimmari10 <kimmari10@naver.com>
2014-10-14 16:05:35 +0900

    3.6.2 queryForInt() p.263 getCount() 함수를 JdbcTemplate의 queryForInt를 사용하여 간소화

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -7,8 +7,11 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.ResultSetExtractor;
 
 import springbook.user.domain.User;
 
@@ -73,40 +76,7 @@ public class UserDao {
 	}
 
 	public int getCount() throws SQLException {
-		Connection c = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-
-		try{
-			c = dataSource.getConnection();
-
-			ps = c.prepareStatement("select count(*) from users");
-
-			rs = ps.executeQuery();
-			rs.next();
-			return rs.getInt(1);
-		} catch (SQLException e){
-			throw e;
-		}finally {
-			if(rs != null){
-				try {
-					rs.close();
-				} catch (SQLException e){
-				}
-			}
-			if(ps != null) {
-				try {
-					ps.close();
-				} catch (SQLException e) {
-				}
-			}
-			if(c != null) {
-				try{
-					c.close();
-				}catch (SQLException e) {
-				}	
-			}
-		}
+		return this.jdbcTemplate.queryForInt("select count(*) from users");
 	}
 	
 
