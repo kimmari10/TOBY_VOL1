9599666f09aaa1eebbfb9795423bd02e7c8eef6c
kimmari10 <kimmari10@naver.com>
2014-10-14 11:02:23 +0900

    3.4.1 JdbcContext의 분리 p. 235 applicationContext.xml 파일 수정 UserDao에 있던 jdbc관련 함수를 클래스로  생성

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -17,7 +17,12 @@ public class UserDao {
 	private Connection c;
 	private User user;
 	private DataSource dataSource;
-	
+	private JdbcContext jdbcContext;
+
+	public void setJdbcContext(JdbcContext jdbcContext) {
+		this.jdbcContext = jdbcContext;
+	}
+
 	public UserDao() {
 	}
 	
@@ -30,7 +35,7 @@ public class UserDao {
 	}
 	
 	public void add(final User user) throws ClassNotFoundException, SQLException {
-		jdbcContextWithStatementStrategy( new StatementStrategy() {
+		this.jdbcContext.workWithStatementStrategy( new StatementStrategy() {
 			public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
 				PreparedStatement ps = c.prepareStatement(
 						"INSERT INTO USERS(ID, NAME, PASSWORD) VALUES(?,?,?)");
@@ -72,7 +77,7 @@ public class UserDao {
 	}
 	
 	public void deleteAll() throws SQLException {
-		jdbcContextWithStatementStrategy(new StatementStrategy() {
+		this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
 			public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
 				return c.prepareStatement("delete from users");
 			}
@@ -116,21 +121,6 @@ public class UserDao {
 			}
 		}
 	}
-	
-	public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
-		Connection c = null;
-		PreparedStatement ps = null;
-		
-		try {
-			c = dataSource.getConnection();
-			ps = stmt.makePreparedStatement(c);
-			ps.executeUpdate();
-		} catch (SQLException e) {
-			throw e;
-		} finally {
-			if (ps != null) { try {ps.close();} catch (SQLException e) {}}
-			if(c != null) { try {c.close();} catch (SQLException e) {}}
-		}
-	}
+
 	
 }
