c55eaf0afd22ff20884665a43a5196bfcad70308
kimmari10 <kimmari10@naver.com>
2014-10-14 14:05:51 +0900

    3.5.3 템플릿/콜백의 응용 p.254 메모장 각 줄의 숫자를 읽어와서 더하기와 곱하기의 연산을 하는 클래스 작성, 중복코드를 분리해 인터페이스를 이용한 템플릿 처리

---
+++ src/main/java/springbook/learningtest/template/Calculator.java
@@ -0,0 +1,55 @@
+package springbook.learningtest.template;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Calculator {
+	public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException {
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(filepath));
+			int ret = callback.doSomethingWithReader(br);
+			return ret;
+		}
+		catch(IOException e) {
+			System.out.println(e.getMessage());
+			throw e;
+		}
+		finally {
+			if(br != null) {
+				try { br.close(); }
+				catch(IOException e) {System.out.println(e.getMessage()); }
+			}
+		}
+	}
+	public Integer calcSum(String filepath) throws IOException {
+		BufferedReaderCallback sumCallback = 
+				new BufferedReaderCallback() {
+					public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+						Integer sum = 0;
+						String line = null;
+						while((line = br.readLine()) != null) {
+							sum += Integer.valueOf(line);
+						}
+						return sum;
+					}
+				};
+				return fileReadTemplate(filepath, sumCallback);
+		}
+	
+	public Integer calcMultiply(String filepath) throws IOException {
+		BufferedReaderCallback multiplyCallback = 
+				new BufferedReaderCallback() {
+					public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+						Integer multiply = 1;
+						String line = null;
+						while((line = br.readLine()) != null) {
+							multiply *= Integer.valueOf(line);
+						}
+						return multiply;
+					}
+				};
+				return fileReadTemplate(filepath, multiplyCallback);
+	}
+}
