07a20a767cc89d0030fa25d555545dacb6514603
kimmari10 <kimmari10@naver.com>
2014-10-16 13:56:30 +0900

    5.1.5 코드 개선 p.338 upgradeLevels() 리팩토링

--- src/main/java/springbook/user/service/UserService.java
+++ src/main/java/springbook/user/service/UserService.java
@@ -17,19 +17,26 @@ public class UserService {
 	public void upgradeLevels() {
 		List<User> users = userDao.getAll();
 		for(User user : users) {
-			boolean changed = false; // 책에는 null
-			if (user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
-				user.setLevel(Level.SILVER);
-				changed = true;
+			if(canUpgradeLevel(user)) {
+				upgradeLevel(user);
 			}
-			else if (user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
-				user.setLevel(Level.GOLD);
-				changed = true;
-			}
-			else if (user.getLevel() == Level.GOLD) { changed = false; }
-			else { changed = false; }
-			if(changed) {userDao.update(user);}
+		}	
+	}
+	
+	public boolean canUpgradeLevel(User user) {
+		Level currentLevel = user.getLevel();
+		switch(currentLevel) {
+			case BASIC : return (user.getLogin() >= 50);
+			case SILVER : return (user.getRecommend() >= 30);
+			case GOLD : return false;
+			default : throw new IllegalArgumentException("Unknown Level : "+ currentLevel);
 		}
+	}
+	
+	public void upgradeLevel(User user) {
+		if(user.getLevel() == Level.BASIC) user.setLevel(Level.SILVER);
+		else if(user.getLevel() == Level.SILVER) user.setLevel(Level.GOLD);
+		userDao.update(user);
 	}
 	
 	public void add(User user) {
