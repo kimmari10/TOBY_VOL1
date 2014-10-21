cf1acb081b3fe989b7fec79391b87536f181d1c2
kimmari10 <kimmari10@naver.com>
2014-10-21 11:45:23 +0900

    5.4.4 테스트 대역 p.394 목 오브젝트를 이용한 테스트

--- src/main/java/springbook/user/service/UserService.java
+++ src/main/java/springbook/user/service/UserService.java
@@ -1,20 +1,12 @@
 package springbook.user.service;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 import javax.sql.DataSource;
 
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
@@ -124,4 +116,5 @@ public class UserService {
 			throw new RuntimeException(e);
 		}*/
 	}
+	
 }
