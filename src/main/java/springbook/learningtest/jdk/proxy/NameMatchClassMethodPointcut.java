aaa242fe4cee9eb3aa2ef12f2e58437466a66b76
kimmari10 <kimmari10@naver.com>
2014-10-28 11:29:34 +0900

    6.5.2 DefaultAdvisorAutoProxyCreator의 적용 p. 483 클래스 필터가 포함된 포인트컷 작성, 빈설정 변경

---
+++ src/main/java/springbook/learningtest/jdk/proxy/NameMatchClassMethodPointcut.java
@@ -0,0 +1,25 @@
+package springbook.learningtest.jdk.proxy;
+
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+import org.springframework.util.PatternMatchUtils;
+
+//클래스 필터가 포함된 포인트 컷
+public class NameMatchClassMethodPointcut extends NameMatchMethodPointcut{
+	public void setMappedClassName(String mappedClassName) {
+		this.setClassFilter(new SimpleClassFilter(mappedClassName));
+	}
+
+	static class SimpleClassFilter implements ClassFilter {
+		String mappedName;
+		
+		private SimpleClassFilter(String mappedName) {
+			this.mappedName = mappedName;
+		}
+		
+		public boolean matches(Class<?> clazz) {
+			//simpleMatch : 와일드카드(*)가 들어간 문자열 비교를 지원하는 스프링의 유틸리티 메소드
+			return PatternMatchUtils.simpleMatch(mappedName, clazz.getSimpleName());
+		}
+	}
+}
