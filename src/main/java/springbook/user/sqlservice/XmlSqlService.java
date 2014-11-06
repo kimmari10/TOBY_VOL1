6e6e35fc53fd981dfbc0153c561534b694761fdc
kimmari10 <kimmari10@naver.com>
2014-11-06 13:34:51 +0900

    7.2.2 XML 파일을 이용하는 SQL 서비스 p.576 생성자 초기화 방법에 HashMap을 사용하는 XmlSqlService클래스 작성

--- src/main/java/springbook/user/sqlservice/XmlSqlService.java
+++ src/main/java/springbook/user/sqlservice/XmlSqlService.java
@@ -1,19 +1,41 @@
 package springbook.user.sqlservice;
 
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
-import springbook.user.sqlservice.jaxb.*;
+
+import javax.xml.bind.*;
+
+import springbook.user.dao.UserDao;
+import springbook.user.sqlservice.jaxb.SqlType;
+import springbook.user.sqlservice.jaxb.Sqlmap;
+
 public class XmlSqlService implements SqlService{
 	private Map<String, String> sqlMap = new HashMap<String, String>();
 
 	public XmlSqlService() {
 		String contextPath = Sqlmap.class.getPackage().getName();
-		
+		try {
+			JAXBContext context = JAXBContext.newInstance(contextPath);
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+			InputStream is = UserDao.class.getResourceAsStream("sqlmap.xml");
+			Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
+			
+			for(SqlType sql : sqlmap.getSql()) {
+				sqlMap.put(sql.getKey(), sql.getValue());
+			}
+		} catch (JAXBException e) {
+			throw new RuntimeException(e);
+		}
 	}
 	
 	public String getSql(String key) throws SqlRetrievalFailureException {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = sqlMap.get(key);
+		if(sql == null) {
+			throw new SqlRetrievalFailureException(key + "를 이용해서 SQL을 찾을 수 없습니다");
+		}
+		else
+			return sql;
 	}
 	
 }
