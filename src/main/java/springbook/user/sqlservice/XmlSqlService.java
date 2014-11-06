f5e3f767eb3886f9b120c28dd7781941d76c86d5
kimmari10 <kimmari10@naver.com>
2014-11-06 16:14:05 +0900

    7.2.3 빈의 초기화 작업 p.579 sqlmapFile 프로퍼티추가, @PostConstruct를 이용한 후처리 초기화 JRE라이브러리 설정 변경

--- src/main/java/springbook/user/sqlservice/XmlSqlService.java
+++ src/main/java/springbook/user/sqlservice/XmlSqlService.java
@@ -4,21 +4,40 @@ import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.xml.bind.*;
+import javax.annotation.PostConstruct;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 
 import springbook.user.dao.UserDao;
 import springbook.user.sqlservice.jaxb.SqlType;
 import springbook.user.sqlservice.jaxb.Sqlmap;
 
 public class XmlSqlService implements SqlService{
+	private String sqlmapFile;
+	
+	public void setSqlmapFile(String sqlmapFile) {
+		this.sqlmapFile = sqlmapFile;
+	}
+
 	private Map<String, String> sqlMap = new HashMap<String, String>();
 
-	public XmlSqlService() {
+	public String getSql(String key) throws SqlRetrievalFailureException {
+		String sql = sqlMap.get(key);
+		if(sql == null) {
+			throw new SqlRetrievalFailureException(key + "를 이용해서 SQL을 찾을 수 없습니다");
+		}
+		else
+			return sql;
+	}
+	
+	@PostConstruct
+	public void loadSql() {
 		String contextPath = Sqlmap.class.getPackage().getName();
 		try {
 			JAXBContext context = JAXBContext.newInstance(contextPath);
 			Unmarshaller unmarshaller = context.createUnmarshaller();
-			InputStream is = UserDao.class.getResourceAsStream("sqlmap.xml");
+			InputStream is = UserDao.class.getResourceAsStream(this.sqlmapFile);
 			Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
 			
 			for(SqlType sql : sqlmap.getSql()) {
@@ -29,13 +48,5 @@ public class XmlSqlService implements SqlService{
 		}
 	}
 	
-	public String getSql(String key) throws SqlRetrievalFailureException {
-		String sql = sqlMap.get(key);
-		if(sql == null) {
-			throw new SqlRetrievalFailureException(key + "를 이용해서 SQL을 찾을 수 없습니다");
-		}
-		else
-			return sql;
-	}
 	
 }
