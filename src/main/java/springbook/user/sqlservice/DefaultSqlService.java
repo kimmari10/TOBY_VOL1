94c74970f486a9bb1fc1ef251755654205f14a85
kimmari10 <kimmari10@naver.com>
2014-11-07 12:41:33 +0900

    7.2.6 디폴트 의존관계 p.596 디폴트 오브젝트를 만들고 빈설정을 최소화

---
+++ src/main/java/springbook/user/sqlservice/DefaultSqlService.java
@@ -0,0 +1,12 @@
+package springbook.user.sqlservice;
+
+
+public class DefaultSqlService extends BaseSqlService{
+
+	
+	public DefaultSqlService() {
+		setSqlReader(new JaxbXmlSqlReader());
+		setSqlRegistry(new HashMapSqlRegistry());
+	}
+
+}
