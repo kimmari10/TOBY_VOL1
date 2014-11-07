2559df93ff9c78b45179ef4a61297b108f2c8206
kimmari10 <kimmari10@naver.com>
2014-11-07 11:43:52 +0900

    7.2.6 디폴트 의존관계 p.593 SqlReader와 SqlRegistry의 독립적인 빈 설정 (순환참조 에러해결)

---
+++ src/main/java/springbook/user/sqlservice/BaseSqlService.java
@@ -0,0 +1,32 @@
+package springbook.user.sqlservice;
+
+import javax.annotation.PostConstruct;
+
+public class BaseSqlService implements SqlService{
+	protected SqlReader sqlReader;
+	protected SqlRegistry sqlRegistry;
+	
+	public void setSqlReader(SqlReader sqlReader) {
+		this.sqlReader = sqlReader;
+	}
+
+
+	public void setSqlRegistry(SqlRegistry sqlRegistry) {
+		this.sqlRegistry = sqlRegistry;
+	}
+
+
+	@Override
+	public String getSql(String key) throws SqlRetrievalFailureException {
+		try { return this.sqlRegistry.findSql(key);}
+		catch (SqlNotFoundException e) {throw new SqlRetrievalFailureException(e);
+		}
+	}
+	
+	@PostConstruct
+	public void loadSql() {
+		this.sqlReader.read(sqlRegistry);
+	}
+	
+
+}
