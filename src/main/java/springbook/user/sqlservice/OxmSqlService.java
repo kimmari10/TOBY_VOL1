15559609ec28f8ec732f11c9f60ebf14ec585ec1
kimmari10 <kimmari10@naver.com>
2014-11-10 11:30:11 +0900

    7.3.2 OXM 서비스 추상화 적용 p.610 BaseSqlService로의 위임을 적용한 OxmSqlService

--- src/main/java/springbook/user/sqlservice/OxmSqlService.java
+++ src/main/java/springbook/user/sqlservice/OxmSqlService.java
@@ -14,6 +14,7 @@ import springbook.user.sqlservice.jaxb.Sqlmap;
 
 public class OxmSqlService implements SqlService{
 	private final OxmSqlReader oxmSqlReader = new OxmSqlReader();
+	private final BaseSqlService baseSqlService = new BaseSqlService();
 
 	private SqlRegistry sqlRegistry = new HashMapSqlRegistry();
 	
@@ -30,13 +31,16 @@ public class OxmSqlService implements SqlService{
 	
 	
 	@PostConstruct
-	public void loadSql() { this.oxmSqlReader.read(this.sqlRegistry); }
+	public void loadSql() {
+		this.baseSqlService.setSqlReader(this.oxmSqlReader);
+		this.baseSqlService.setSqlRegistry(this.sqlRegistry);
+		this.baseSqlService.loadSql();
+	}
 	
 	
 	@Override
 	public String getSql(String key) throws SqlRetrievalFailureException {
-		try { return this.sqlRegistry.findSql(key); }
-		catch(SqlNotFoundException e) { throw new SqlRetrievalFailureException(e); }
+		return this.baseSqlService.getSql(key);
 	}
 
 	
