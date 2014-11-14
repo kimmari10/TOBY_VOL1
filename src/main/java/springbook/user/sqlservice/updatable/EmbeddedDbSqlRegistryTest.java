05f86bc3cb6c733e574d75a72a8fd8f0136575c6
kimmari10 <kimmari10@naver.com>
2014-11-14 15:25:37 +0900

    7.5.2 내장형 데이터베이스를 이용한 SQL레지스트리 만들기 p.640 XML 설정을 통한 내장형 DB의 생성과 적용

---
+++ src/main/java/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistryTest.java
@@ -0,0 +1,33 @@
+package springbook.user.sqlservice.updatable;
+
+import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.HSQL;
+
+import org.junit.After;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+
+import springbook.issuetracker.sqlservice.EmbeddedDbSqlRegistry;
+import springbook.issuetracker.sqlservice.UpdatableSqlRegistry;
+
+public class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest{
+	EmbeddedDatabase db;
+	
+	protected UpdatableSqlRegistry createUpdatableSqlRegistry() {
+		db = new EmbeddedDatabaseBuilder()
+		.setType(HSQL).addScript(
+				"classpath:/springbook/user/sqlservice/updatable/sqlRegistrySchema.sql")
+				.build();
+		
+		EmbeddedDbSqlRegistry embeddedDbSqlRegistry = new EmbeddedDbSqlRegistry();
+		embeddedDbSqlRegistry.setDataSource(db);
+		
+		return embeddedDbSqlRegistry;
+				
+	}
+	
+	
+	@After
+	public void tearDown() {
+		db.shutdown();
+	}
+}
