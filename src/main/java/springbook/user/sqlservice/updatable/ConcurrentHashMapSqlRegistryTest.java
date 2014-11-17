8d4266968e7b4eb6fe2282e802efd3f4e3eee295
kimmari10 <kimmari10@naver.com>
2014-11-17 11:05:01 +0900

    7.5.3 트랜잭션 적용 p.644 트랜잭션 기능을 가진 EmbeddedDbSqlRegistry

--- src/main/java/springbook/user/sqlservice/updatable/ConcurrentHashMapSqlRegistryTest.java
+++ src/main/java/springbook/user/sqlservice/updatable/ConcurrentHashMapSqlRegistryTest.java
@@ -1,5 +1,11 @@
 package springbook.user.sqlservice.updatable;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import springbook.issuetracker.sqlservice.SqlUpdateFailureException;
 import springbook.issuetracker.sqlservice.UpdatableSqlRegistry;
 
 public class ConcurrentHashMapSqlRegistryTest extends AbstractUpdatableSqlRegistryTest{
@@ -8,6 +14,22 @@ public class ConcurrentHashMapSqlRegistryTest extends AbstractUpdatableSqlRegist
 	@Override
 	protected UpdatableSqlRegistry createUpdatableSqlRegistry() {
 		return new ConcurrentHashMapSqlRegistry();
+	}
+	
+	@Test
+	public void transacionUpdate() {
+		checkFind("SQL1", "SQL2", "SQL3");
+		
+		Map<String, String> sqlmap = new HashMap<String, String>();
+		sqlmap.put("KEY1", "Modified1");
+		sqlmap.put("KEY9999!@#$", "Modified9999");
+		// 존재하지않는 키 설정으로 테스트실패
+		
+		try {
+			sqlRegistry.updateSql(sqlmap);
+//			fail();
+		} catch (SqlUpdateFailureException e) {}
+		checkFind("SQL1", "SQL2", "SQL3");
 	}
 
 }
--- src/main/java/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistry.java
+++ src/main/java/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistry.java
@@ -6,6 +6,10 @@ import javax.sql.DataSource;
 
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import springbook.issuetracker.sqlservice.SqlUpdateFailureException;
 import springbook.issuetracker.sqlservice.UpdatableSqlRegistry;
@@ -13,9 +17,13 @@ import springbook.user.sqlservice.SqlNotFoundException;
 
 public class EmbeddedDbSqlRegistry implements UpdatableSqlRegistry{
 	SimpleJdbcTemplate jdbc;
+	TransactionTemplate transactionTemplate;
+	//멀티스레드 환경에서 공유가능
 	
 	public void setDataSource(DataSource dataSource) {
 		jdbc = new SimpleJdbcTemplate(dataSource);
+		transactionTemplate = new TransactionTemplate(
+				new DataSourceTransactionManager(dataSource));
 	}
 	
 	public void registerSql(String key, String sql) {
@@ -37,9 +45,15 @@ public class EmbeddedDbSqlRegistry implements UpdatableSqlRegistry{
 		}
 	}
 	
-	public void updateSql(Map<String, String> sqlmap) throws SqlUpdateFailureException {
-		for(Map.Entry<String, String> entry : sqlmap.entrySet()) {
-			updateSql(entry.getKey(), entry.getValue());
-		}
+	public void updateSql(final Map<String, String> sqlmap) throws SqlUpdateFailureException {
+		transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+			
+			@Override
+			protected void doInTransactionWithoutResult(TransactionStatus status) {
+				for(Map.Entry<String, String> entry : sqlmap.entrySet()) {
+					updateSql(entry.getKey(), entry.getValue());
+				}
+			}
+		});
 	}
 }
