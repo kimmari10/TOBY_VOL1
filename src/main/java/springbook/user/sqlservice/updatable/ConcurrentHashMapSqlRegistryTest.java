9795ac14867fa6a0c162139ac131a602b46ded8f
kimmari10 <kimmari10@naver.com>
2014-11-17 13:09:48 +0900

    다른파일에 작성된 코드 이전, 중복파일삭제(임포트문제해결)

--- src/main/java/springbook/user/sqlservice/updatable/ConcurrentHashMapSqlRegistryTest.java
+++ src/main/java/springbook/user/sqlservice/updatable/ConcurrentHashMapSqlRegistryTest.java
@@ -1,11 +1,5 @@
 package springbook.user.sqlservice.updatable;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.junit.Test;
-
-import springbook.issuetracker.sqlservice.SqlUpdateFailureException;
 import springbook.issuetracker.sqlservice.UpdatableSqlRegistry;
 
 public class ConcurrentHashMapSqlRegistryTest extends AbstractUpdatableSqlRegistryTest{
@@ -16,20 +10,4 @@ public class ConcurrentHashMapSqlRegistryTest extends AbstractUpdatableSqlRegist
 		return new ConcurrentHashMapSqlRegistry();
 	}
 	
-	@Test
-	public void transacionUpdate() {
-		checkFind("SQL1", "SQL2", "SQL3");
-		
-		Map<String, String> sqlmap = new HashMap<String, String>();
-		sqlmap.put("KEY1", "Modified1");
-		sqlmap.put("KEY9999!@#$", "Modified9999");
-		// 존재하지않는 키 설정으로 테스트실패
-		
-		try {
-			sqlRegistry.updateSql(sqlmap);
-//			fail();
-		} catch (SqlUpdateFailureException e) {}
-		checkFind("SQL1", "SQL2", "SQL3");
-	}
-
 }
