1f95245181670af6ae5890a1c3725188253c21b4
kimmari10 <kimmari10@naver.com>
2014-11-18 11:40:12 +0900

    DataSource 설정 이전

--- src/main/java/springbook/user/dao/TestApplicationContext.java
+++ src/main/java/springbook/user/dao/TestApplicationContext.java
@@ -1,10 +1,24 @@
 package springbook.user.dao;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 @Configuration
 @ImportResource("/mysql.xml")
 public class TestApplicationContext {
+	@Bean
+	public DataSource dataSource() {
+		SimpleDriverDataSource ds = new SimpleDriverDataSource();
+		ds.setDriverClass(com.mysql.jdbc.Driver.class);
+		ds.setUrl("jdbc:mysql://localhost/jsjs");
+		ds.setUsername("js");
+		ds.setPassword("kim");
+		
+		return ds;
+	}
 
 }
