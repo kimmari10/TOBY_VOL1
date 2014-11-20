d0ae2fac3957790f0b5b59468fdbae57b58be066
kimmari10 <kimmari10@naver.com>
2014-11-20 16:07:23 +0900

    7.6.3 컨텍스트 분리와 @Import p.687 @Import를 이용해 Sql관련 설정정보 분리

---
+++ src/main/java/springbook/user/dao/SqlServiceContext.java
@@ -0,0 +1,61 @@
+package springbook.user.dao;
+import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.HSQL;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.mail.MailSender;
+import org.springframework.oxm.Unmarshaller;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import springbook.user.service.DummyMailSender;
+import springbook.user.service.UserService;
+import springbook.user.service.UserServiceTest.TestUserService;
+import springbook.user.sqlservice.OxmSqlService;
+import springbook.user.sqlservice.SqlRegistry;
+import springbook.user.sqlservice.SqlService;
+import springbook.user.sqlservice.updatable.EmbeddedDbSqlRegistry;
+
+@Configuration
+public class SqlServiceContext {
+	
+	@Bean
+	public SqlService sqlService() {
+		OxmSqlService sqlService = new OxmSqlService();
+		sqlService.setUnmarshaller(unmarshaller());
+		sqlService.setSqlRegistry(sqlRegistry());
+		return sqlService;
+	}
+	
+	@Bean 
+	public Unmarshaller unmarshaller() {
+		Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+		marshaller.setContextPath("springbook.user.sqlservice.jaxb");
+		return marshaller;
+	}
+	
+	@Bean
+	public SqlRegistry sqlRegistry() {
+		EmbeddedDbSqlRegistry sqlRegistry = new EmbeddedDbSqlRegistry();
+		sqlRegistry.setDataSource(embeddedDatabase());
+		return sqlRegistry;
+	}
+	
+	@Bean
+	public DataSource embeddedDatabase() {
+		return new EmbeddedDatabaseBuilder()
+		.setName("embeddedDatabase")
+		.setType(HSQL)
+		.addScript(
+				"classpath:/springbook/user/sqlservice/updatable/sqlRegistrySchema.sql")
+				.build();
+	}
+}
